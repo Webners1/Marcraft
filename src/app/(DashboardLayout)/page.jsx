8c02baa7d2809e0811bc1e69e4b1c01fe43f4bd4
Hsnmughal <hassanmughal12386@gmail.com>
2024-10-24 02:33:46 +0500

    refactoring code

--- src/app/(DashboardLayout)/page.jsx
+++ src/app/(DashboardLayout)/page.jsx
@@ -5,14 +5,10 @@ import PageContainer from '@/app/components/container/PageContainer';
 
 // components
 import Banner from '@/app/components/landingpage/banner/Banner';
-import C2a from '@/app/components/landingpage/c2a/C2a';
 import C2a2 from '@/app/components/landingpage/c2a/C2a2';
 import DemoSlider from '@/app/components/landingpage/demo-slider/DemoSlider';
-import Features from '@/app/components/landingpage/features/Features';
 import Footer from '@/app/components/landingpage/footer/Footer';
-import Frameworks from '@/app/components/landingpage/frameworks/Frameworks';
 import LpHeader from '@/app/components/landingpage/header/Header';
-import Testimonial from '@/app/components/landingpage/testimonial/Testimonial';
 
 export default function Landingpage() {
   return (
@@ -20,10 +16,6 @@ export default function Landingpage() {
       <LpHeader />
       <Banner />
       <DemoSlider />
-      {/* <Frameworks /> */}
-      {/* <Testimonial />
-      <Features /> */}
-      {/* <C2a /> */}
       <C2a2 />
       <Footer />
     </PageContainer>
